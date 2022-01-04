04f064e8bc44e1bf55c24bf8d0b86ac6d5b20f62
Dany Colin <danyiel5978@gmail.com>
2022-01-03 18:30:51 -0600

    1_3:preprocessor-0 first commit

--- 1_1:functions-0/src/main.c
+++ 1_1:functions-0/src/main.c
@@ -2,7 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct cmplx {
+	int re;
+	int im;
+} cmplx;
+
+cmplx suma(cmplx a, cmplx b) {
+	cmplx answer;
+	answer.re = a.re + b.re;
+	answer.im = a.im + b.im;
+	return answer;
+}
+
+void print_cmplx(cmplx a) {
+	printf("Real: %d, Im: %d\n", a.re, a.im);
+}
+
 int main(int argc, char **argv) {
-	printf("Hello %s!\n", argv[1]);
+	cmplx a = { .re = 3, .im = -5 };
+	cmplx b = { .re = 2, .im = 4 };
+	print_cmplx(suma(a, b));
 	return EXIT_SUCCESS;
 }
