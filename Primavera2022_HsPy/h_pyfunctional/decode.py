9afc7673491deed290617d8ee189e67ce31c07ec
Danyiel Colin <danyiel5978@gmail.com>
2022-05-22 00:48:23 -0500

    modified:   ../b_statelist/StateFunctions.hs new file:   1ring2rule.txt modified:   __pycache__/decode.cpython-310.pyc modified:   decode.py

--- Primavera2022_HsPy/h_pyfunctional/decode.py
+++ Primavera2022_HsPy/h_pyfunctional/decode.py
@@ -3,15 +3,36 @@
 import sys
 
 
-def decode(filename) -> str:
+def decode_bin(filename) -> str:
     letters = []
     with open(filename, "r") as file:
         for line in file.readlines():
-            chrs_line = map(lambda b: chr(int(b, 2)), line.split())
-            letters.extend(list(chrs_line))
+            try:
+                chrs_line = map(lambda b: chr(int(b, 2)), line.split())
+                letters.extend(list(chrs_line))
+            except ValueError:
+                break
     return "".join(letters)
 
 
+def encode_bin(message, filename, words_perline=6):
+    message = list(message.replace("\n", " "))
+    space_code = "20"  # "00100000"
+    with open(filename, "w") as file:
+        while message:
+            line = []
+            for i in range(words_perline):
+                try:
+                    bin_code = "{0:02x}".format(ord(message.pop(0)))
+                    line.append(bin_code)
+                except IndexError:
+                    line.extend([space_code] * (words_perline - i))
+                    break
+            file.write(" ".join(line) + "\n")
+
+
 if __name__ == "__main__":
-    msg = decode(sys.argv[1])
-    print(msg.replace(". ", ".\n").strip())
+    # msg = decode_bin(sys.argv[1])
+    # print(msg.replace(". ", ".\n").strip())
+    with open(sys.argv[1], "r") as file:
+        encode_bin(file.read(), sys.argv[2], 32)
